66164102b3094c08cf051e141046643ef338f031
Berenicio <bernardo.ssilva0808@gmail.com>
2023-05-21 01:40:57 -0300

    created a component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RedesComponent } from './redes/redes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RedesComponent
   ],
   imports: [
     BrowserModule,
